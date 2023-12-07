0f3b57783a2739897b88b3fd9d7090fcfb46b542
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2023-12-07 09:05:08 +0800

    Add relationships for books and reviews in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,16 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *

a09aa268e50833ada6b9a914923b088dfde5c6e0
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2023-12-07 09:17:46 +0800

    Add foreign key constraint to books table

--- database/migrations/2023_11_29_072825_create_books_table.php
+++ database/migrations/2023_11_29_072825_create_books_table.php
@@ -11,12 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('books', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->string('author');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('books')) {
+            Schema::create('books', function (Blueprint $table) {
+                $table->id();
+                $table->string('title');
+                $table->string('author');
+                $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+                $table->timestamps();
+            });
+        }
     }
 
     /**

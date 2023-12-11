bf331744acc5c410e392cd92372b61ef5ab6f9f0
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2023-12-11 15:02:34 +0800

    Add rate limiter and update routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use Illuminate\Cache\RateLimiter;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\ReviewController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,12 +21,21 @@ use App\Http\Controllers\ReviewController;
 Route::redirect('/', '/books', 301);
 
 Route::resource('books', BookController::class);
+Route::middleware(['non_auth_review_throttle', 'auth_review_throttle'])->group(function () {
+  Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create', 'store']);
+});
 
-Route::resource('books.reviews', ReviewController::class)
-  ->scoped(['review' => 'book'])
-  ->only(['create', 'store']);
+Route::controller(UserController::class)->group(function () {
+  Route::get('register', 'register')->name('register.form');
+  Route::post('register', 'store')->name('register.store');
+  Route::get('login', 'login')->name('login');
+  Route::post('login', 'authenticate')->name('login.authenticate');
+  Route::get('logout', 'logout')->middleware('auth')->name('logout');
+});
 
-Route::get('register', [UserController::class, 'register'])->name('register.form');
-Route::post('register', [UserController::class, 'store'])->name('register.store');
-Route::get('login', [UserController::class, 'login'])->name('login');
-Route::post('login', [UserController::class, 'authenticate'])->name('login.authenticate');
+// ! for testing purposes
+Route::middleware('reset_throttle')->get('/reset-throttle', function () {
+  return "Throttle reset!!!";
+});

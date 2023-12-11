eda82c740de36c9f7f1db7d1ca13fc9d67751b20
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2023-12-11 15:02:19 +0800

    Refactor CSS styles in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,73 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <title>Book Reviews</title>
     @vite('resources/css/app.css')
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio"></script>
     <style type="text/tailwindcss">
-    .btn {
-      @apply bg-white rounded-md px-4 py-2 text-center font-medium text-slate-500 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50 h-10;
-    }
+        .btn {
+            @apply bg-white rounded-md px-4 py-2 text-center font-medium text-slate-500 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50 h-10;
+        }
 
-    .input {
-      @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none rounded-md border-slate-300;
-    }
-    
-    .border-red-500 {
-    border-color: #f56565;
-    }
+        .input {
+            @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none rounded-md border-slate-300;
+        }
 
-    .filter-container {
-      @apply mb-4 flex space-x-2 rounded-md bg-slate-100 p-2;
-    }
+        .border-red-500 {
+            border-color: #f56565;
+        }
 
-    .filter-item {
-      @apply flex w-full items-center justify-center rounded-md px-4 py-2 text-center text-sm font-medium text-slate-500;
-    }
+        .filter-container {
+            @apply mb-4 flex space-x-2 rounded-md bg-slate-100 p-2;
+        }
 
-    .filter-item-active {
-      @apply bg-white shadow-sm text-slate-800 flex w-full items-center justify-center rounded-md px-4 py-2 text-center text-sm font-medium;
-    }
+        .filter-item {
+            @apply flex w-full items-center justify-center rounded-md px-4 py-2 text-center text-sm font-medium text-slate-500;
+        }
 
-    .book-item {
-      @apply text-sm rounded-md bg-white p-4 leading-6 text-slate-900 shadow-md shadow-black/5 ring-1 ring-slate-700/10;
-    }
+        .filter-item-active {
+            @apply bg-white shadow-sm text-slate-800 flex w-full items-center justify-center rounded-md px-4 py-2 text-center text-sm font-medium;
+        }
 
-    .book-title {
-      @apply text-lg font-semibold text-slate-800 hover:text-slate-600;
-    }
+        .book-item {
+            @apply text-sm rounded-md bg-white p-4 leading-6 text-slate-900 shadow-md shadow-black/5 ring-1 ring-slate-700/10;
+        }
 
-    .book-author {
-      @apply block text-slate-600;
-    }
+        .book-title {
+            @apply text-lg font-semibold text-slate-800 hover:text-slate-600;
+        }
 
-    .book-rating {
-      @apply text-sm font-medium text-slate-700;
-    }
+        .book-author {
+            @apply block text-slate-600;
+        }
 
-    .book-review-count {
-      @apply text-xs text-slate-500;
-    }
+        .book-rating {
+            @apply text-sm font-medium text-slate-700;
+        }
 
-    .empty-book-item {
-      @apply text-sm rounded-md bg-white py-10 px-4 text-center leading-6 text-slate-900 shadow-md shadow-black/5 ring-1 ring-slate-700/10;
-    }
+        .book-review-count {
+            @apply text-xs text-slate-500;
+        }
 
-    .empty-text {
-      @apply font-medium text-slate-500;
-    }
+        .empty-book-item {
+            @apply text-sm rounded-md bg-white py-10 px-4 text-center leading-6 text-slate-900 shadow-md shadow-black/5 ring-1 ring-slate-700/10;
+        }
 
-    .reset-link {
-      @apply text-slate-500 underline;
-    }
+        .empty-text {
+            @apply font-medium text-slate-500;
+        }
+
+        .reset-link {
+            @apply text-slate-500 underline;
+        }
     </style>
     <script src="//unpkg.com/alpinejs" defer></script>
-</head>
-<body class="container mx-auto mt-10 mb-10 max-w-3xl">
-  <x-success />
+  </head>
+<body class="pt-16 mx-auto container">
+    <x-success />
+    <x-navbar />
     @yield('content')
+    <div class="fixed bottom-0 left-0 right-0 bg-blue-500 text-white text-center py-3">
+      Care to publish and rate your book? <a href="" class="underline">Register</a> or <a href="" class="underline">Login</a> now!
+  </div>
 </body>
 
 </html>

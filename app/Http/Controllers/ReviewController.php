abf307ac4e5e2420af29c4f142121383e0e030a2
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2023-12-11 14:56:19 +0800

    Refactor ReviewController store method to include user_id and visitor_ip

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,15 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('throttle:reviews')->only(['store']);
-    }
-
     /**
      * Display a listing of the resource.
      */
@@ -35,18 +31,27 @@ class ReviewController extends Controller
      */
     public function store(Request $request, Book $book)
     {
+
         $validated = $request->validate([
             'review' => 'required|min:15',
             'rating' => 'required|numeric|between:1,5',
         ]);
 
-        $book->reviews()->create($validated);
+        // $book->reviews()->create($validated);
+        $data = [
+            'review' => $validated['review'],
+            'rating' => $validated['rating'],
+            'user_id' => auth()->check() ? auth()->id() : null,
+            'visitor_ip' => $request->ip(),
+        ];
+
+        $book->reviews()->create($data);
 
         return redirect()->route('books.show', $book)->with('success', 'Review was added');
     }
 
 
-    /**
+    /**s
      * Display the specified resource.
      */
     public function show(string $id)

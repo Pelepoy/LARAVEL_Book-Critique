a2a1d8ff36b1bddfad9c6448fd9c497e0fe71bbe
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2023-12-11 14:55:06 +0800

    Add route middlewares for throttling

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
@@ -64,5 +64,12 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    ];
+
+    protected $routeMiddleware = [
+        // Other middleware...
+        'non_auth_review_throttle' => \App\Http\Middleware\NonAuthReviewRateLimiter::class,
+        'auth_review_throttle' => \App\Http\Middleware\AuthReviewRateLimiter::class,
+        'reset_throttle' => \App\Http\Middleware\ResetThrottleMiddleware::class,
     ];
 }

81fd248fb9a4c0f77341b69e4bbb81134c2e596c
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2023-12-11 08:46:48 +0800

    Update nullable user_id and email_verified_at properties

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -52,11 +52,12 @@ namespace App\Models{
  *
  * @property int $id
  * @property int $book_id
- * @property int $user_id
+ * @property int|null $user_id
  * @property string $review
  * @property int $rating
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User|null $author
  * @property-read \App\Models\Book $book
  * @method static \Database\Factories\ReviewFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|Review newModelQuery()
@@ -80,7 +81,7 @@ namespace App\Models{
  * @property int $id
  * @property string $name
  * @property string $email
- * @property string|null $email_verified
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
  * @property mixed $password
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
@@ -99,7 +100,7 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|User query()
  * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerified($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
